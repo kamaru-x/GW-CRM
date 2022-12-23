2fdbfca3191330537604458ebdfeb40f3c177c74
KAMARU <xin.kamaru@gmail.com>
2022-12-23 23:53:15 +0530

    project partially completed

--- d_auth/forms.py
+++ d_auth/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 from d_auth.models import User
 
 class LoginForm(forms.Form):
@@ -11,3 +11,11 @@ class CreateCustomer(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2','Country','CCode','Mobile')
+
+
+class ChangePassword(PasswordChangeForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        for fieldname in ['old_password','new_password1','new_password2'] :
+            self.fields[fieldname].widget.attrs = {'class' : 'form-control'}
--- d_auth/urls.py
+++ d_auth/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from d_auth import views
+from django.urls import path,reverse_lazy
+from django.contrib.auth.views import PasswordChangeView,PasswordChangeDoneView
+from d_auth.forms import ChangePassword
 
 urlpatterns = [
     path('create-customer',views.create_customer,name='create-customer'),
     path('',views.user_login,name='user-login'),
+    path('change-admin-password/',PasswordChangeView.as_view(template_name='aut/change-admin-password.html',success_url=reverse_lazy('password changed'),form_class=ChangePassword),name='change-admin-password'),
+    path('change-password/',PasswordChangeView.as_view(template_name='aut/change-customer-password.html',success_url=reverse_lazy('password changed'),form_class=ChangePassword),name='change-customer-password'),
 ]
